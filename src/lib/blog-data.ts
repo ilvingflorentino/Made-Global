ca35e21c4e6dd756d98879e51f006274fa703743
Belckrop Agencia de IA <gestion@belckrop.com>
2025-05-24 17:53:43 +0000

    en la ruta public/images/ estan las imagenes blog-article-1.svg, blog-article-2.svg, y blog-article-3.svg

--- src/lib/blog-data.ts
+++ src/lib/blog-data.ts
@@ -33,11 +33,12 @@ interface Dictionary {
 
 
 export const placeholderArticlesData: BlogArticle[] = [
-  { id: 1, titleKey: "blogArticle1Title", excerptKey: "blogArticle1Excerpt", fullContentKey: "blogArticle1Full", imageUrl: "/images/blog-article-1.jpg", dataAiHint: "woodworking tools", date: "2024-07-15" },
-  { id: 2, titleKey: "blogArticle2Title", excerptKey: "blogArticle2Excerpt", fullContentKey: "blogArticle2Full", imageUrl: "/images/blog-article-2.jpg", dataAiHint: "forest sustainable", date: "2024-07-10" },
-  { id: 3, titleKey: "blogArticle3Title", excerptKey: "blogArticle3Excerpt", fullContentKey: "blogArticle3Full", imageUrl: "/images/blog-article-3.jpg", dataAiHint: "wood varnish", date: "2024-07-05" },
+  { id: 1, titleKey: "blogArticle1Title", excerptKey: "blogArticle1Excerpt", fullContentKey: "blogArticle1Full", imageUrl: "/images/blog-article-1.svg", dataAiHint: "woodworking tools", date: "2024-07-15" },
+  { id: 2, titleKey: "blogArticle2Title", excerptKey: "blogArticle2Excerpt", fullContentKey: "blogArticle2Full", imageUrl: "/images/blog-article-2.svg", dataAiHint: "forest sustainable", date: "2024-07-10" },
+  { id: 3, titleKey: "blogArticle3Title", excerptKey: "blogArticle3Excerpt", fullContentKey: "blogArticle3Full", imageUrl: "/images/blog-article-3.svg", dataAiHint: "wood varnish", date: "2024-07-05" },
 ];
 
 export function getArticleById(id: number): BlogArticle | undefined {
   return placeholderArticlesData.find(article => article.id === id);
 }
+
